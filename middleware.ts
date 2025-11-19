fb193c3fdaa0914e1ea160372c6ac85c71312eac
sabbirlog <mridhasabbir5@gmail.com>
2025-11-19 09:42:59 +0600

    feat: middleware

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/") {
+    const token = req.cookies.get("auth_token")?.value;
+
+    if (token) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    } else {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [String.raw`/((?!api|_next|.*\..*).*)`],
+};
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  skipProxyUrlNormalize: true,
   productionBrowserSourceMaps: false,
   reactStrictMode: false,
   devIndicators: false,
